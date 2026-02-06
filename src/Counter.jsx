bd8bc0075fad6b49a04bba22942098e376f6748a
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-02-06 16:25:21 +0530

    again minor issue resolved

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,7 +4,7 @@ const Counter = () => {
   const [count, setCount] = useState(0);
   return <>
   <h1>Counter App</h1>
-  <p>Count:{count}</p>
+  <p>Count: {count}</p>
   <button onClick={()=> setCount((prev)=> prev+1)}>Increment</button>
   <button onClick={()=> setCount((prev)=> prev-1)}>Decrement</button>
   </>;
